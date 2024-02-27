c333c04e69d1651ede81f9dc406cc1b7a4068323
Ishaan Paul <ishaanpaul@outlook.com>
2024-02-27 14:40:41 -0800

    Started drawing circle using mouse events

---
+++ Course/Drawing with OpenCV/drawing.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def drawCircle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), radius=100, color=(0,255,0), thickness=-1)
+cv2.namedWindow(winname='drawing')
+cv2.setMouseCallback('drawing', drawCircle)
+
+img = np.zeros((512,512,3), np.int8)
+
+while True:
+    cv2.imshow('drawing', img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
