650ea2bdadcb6599faf3a5c4b554f0dc2e566536
Ishaan Paul <ishaanpaul@outlook.com>
2024-02-27 16:09:25 -0800

    Drawing with multiple events

--- Course/Drawing with OpenCV/drawing.py
+++ Course/Drawing with OpenCV/drawing.py
@@ -3,6 +3,8 @@ import numpy as np
 
 def drawCircle(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), radius=100, color=(0,0,255), thickness=-1)
+    elif event == cv2.EVENT_RBUTTONDOWN:
         cv2.circle(img, (x, y), radius=100, color=(0,255,0), thickness=-1)
 cv2.namedWindow(winname='drawing')
 cv2.setMouseCallback('drawing', drawCircle)
@@ -14,4 +16,4 @@ while True:
     if cv2.waitKey(20) & 0xFF == 27:
         break
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
