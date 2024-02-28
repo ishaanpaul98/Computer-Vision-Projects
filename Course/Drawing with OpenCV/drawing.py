ed8d86eb3e95b6ce7e95792f055d6f118fc5bfe5
Ishaan Paul <ishaanpaul@outlook.com>
2024-02-27 16:53:20 -0800

    Drawing with events to detect and draw on mouse drags

--- Course/Drawing with OpenCV/drawing.py
+++ Course/Drawing with OpenCV/drawing.py
@@ -1,13 +1,32 @@
 import cv2
 import numpy as np
 
+#True while mouse button down and false while mouse button up
+drawing = False
+ix = -1
+iy = -1
+
 def drawCircle(event, x, y, flags, param):
+    #if event == cv2.EVENT_LBUTTONDOWN:
+    #    cv2.circle(img, (x, y), radius=100, color=(0,0,255), thickness=-1)
+    #elif event == cv2.EVENT_RBUTTONDOWN:
+    #    cv2.circle(img, (x, y), radius=100, color=(0,255,0), thickness=-1)
+    pass
+
+def drawRectangle(event, x, y, flags, param):
+    global ix, iy, drawing
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, (x, y), radius=100, color=(0,0,255), thickness=-1)
-    elif event == cv2.EVENT_RBUTTONDOWN:
-        cv2.circle(img, (x, y), radius=100, color=(0,255,0), thickness=-1)
+        drawing = True
+        ix, iy = x,y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            cv2.rectangle(img, (ix, iy), (x,y), (0,255,0), -1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img, (ix, iy), (x,y), (0,255,0), -1)
+
 cv2.namedWindow(winname='drawing')
-cv2.setMouseCallback('drawing', drawCircle)
+cv2.setMouseCallback('drawing', drawRectangle)
 
 img = np.zeros((512,512,3), np.int8)
 
